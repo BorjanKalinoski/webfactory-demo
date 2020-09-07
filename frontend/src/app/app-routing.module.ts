1e4dcc52104d0c09a46520fd20b635c5f022fef7
Bojran Kalinoski <drsmrtnik@localhost.localdomain>
2020-09-07 20:32:07 +0200

    Implement basic routing, code refactor

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {RegisterComponent} from './components/register-form/register.component';
+import {LoginComponent} from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: RegisterComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
